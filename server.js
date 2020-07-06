405952e44bdf8202847aed072b2a361578b6986b
Phencharaud S Thach <pshannonthach@gmail.com>
2020-07-06 18:00:36 -0400

    connected to database and building routes/models

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/pages', require('./routes/api/pages'));
 
 const PORT = process.env.PORT || 5000;
 
